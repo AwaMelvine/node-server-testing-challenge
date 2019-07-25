c038615f3eb303cfad9c22100e553bf3aafc24d6
Awa Melvine <melvineawa@gmail.com>
2019-07-25 13:08:01 +0100

    Create post model boilerplate code

---
+++ models/Post.js
@@ -0,0 +1,15 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    async get(id = null) {
+        return null;
+    },
+
+    async add(post) {
+        return null;
+    },
+
+    async delete(id) {
+        return null;
+    }
+}
