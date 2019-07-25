eb0e647fec6619274d7b37b77d9ce0d75b93225e
Awa Melvine <melvineawa@gmail.com>
2019-07-25 13:20:22 +0100

    Create tests for post model

--- models/Post.js
+++ models/Post.js
@@ -9,7 +9,7 @@ module.exports = {
         return null;
     },
 
-    async delete(id) {
+    async remove(id) {
         return null;
     }
 }
---
+++ models/Post.spec.js
@@ -0,0 +1,31 @@
+const db = require('../data/dbConfig');
+const Post = require('../models/Post');
+
+describe('Post Model', () => {
+    describe('get()', () => {
+        it('returns json ', () => {
+
+        });
+        it('returns an array when no arguments supplied', () => {
+
+        });
+        it('returns object when id is specified', () => {
+
+        });
+    });
+
+    describe('add()', () => {
+        it('creates a post', () => {
+
+        });
+        it('returns an object', () => {
+
+        });
+    });
+
+    describe('remove()', () => {
+        it('deletes a post', () => {
+
+        });
+    });
+});
